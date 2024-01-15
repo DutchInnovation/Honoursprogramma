5fb02fcd825897f53b28613e946278dae7441e2a
R1ch4rd <32466820+DutchInnovation@users.noreply.github.com>
2024-01-15 17:10:42 +0100

    setup
    setup

---
+++ aanvragen.php
@@ -0,0 +1,25 @@
+<!-- html template -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- link bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- link font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<!-- body template -->
+<body style="background-color:rgb(238,236,222);">
+    <div class="text">
+    <h1 class>Buurtbag aanvragen</h1>
+    <strong>Uniek kans om de Buurtbag aan te vragen</strong>
+        Deze tas is niet zomaar een tasl het is een speciale manier om nieuwe gezichten te verwelkomen en de gemeenschapsbanden te versterken.
+
+        Dus, of je nu nieuw bent in de buurt of hier al langere tijd woont, de Buurtbag is er voor jou!
+        Aarzel niet om je Buurtbag aan te vragen en ontdek de verassingen die op je wachten. Samen maken we de Bloemenbuurt een nog waarmee en gezelligere plek!
+    </p>
+    <button type="button" class="btn btn-primary">Aanvragen</button>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,10 +4,30 @@
 <head>
     <title>Page Title</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- link bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- link font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
 <body>
-    <h1>My First Heading</h1>
-    <p>My first paragraph.</p>
+    <div class="left-side">
+        <h1 class="title">Bloemenbuurt <br> Whatsapp</h1>
+    </div>
+    <div class="right-side">
+        <!-- ... (your existing HTML) ... -->
+<div class="inlog-container">
+    <h1 class="title">right side</h1>
+    <form action="login.php" method="post">
+        <input type="text" name="username" placeholder="Username" required> <br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <a href="aanvragen.php" type="button"> Aanvraag </a> <br>
+    </form>
+</div>
+<!-- ... (your existing HTML) ... -->
+
+        <img src="Flowers.png" alt="Image" class="bottom-right-image">        
+    </div>
+
 </body>
 </html>
 
